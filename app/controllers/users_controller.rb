ceded2fa581445e7f29d57adbc0ac3a3e9c8a61f
amarnath-reddy-0-9-1-2 <122003102@sastra.ac.in>
2020-04-01 15:49:37 +0530

    added login method for users as users/login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,15 @@ class UsersController < ApplicationController
     )
     render plain: "Hey! welcome you registered successfully with an id #{new_user.id}"
   end
+
+  def login
+    check1 = params[:user_email]
+    check2 = params[:user_password]
+    user = User.find { |u| u.user_email == check1 }
+    if user.user_password == check2
+      render plain: "Your login is successful!! Welcome #{user.user_name}"
+    else
+      render plain: "incorrect credentials!! Try again"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   #get "todos", to: "todos#index"
   #post "todos", to: "todos#create"
   #get "todos/:id", to: "todos#show"
-
+  post "users/login", to: "users#login"
   resources :todos
   resources :users
+
 end
