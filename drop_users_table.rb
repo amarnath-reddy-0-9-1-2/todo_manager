8c4f2f697607f421b82be57d7f89efcc78bc662b
amarnath-reddy-0-9-1-2 <122003102@sastra.ac.in>
2020-04-03 20:38:26 +0530

    Updated database migrations

--- drop_users_table.rb
+++
@@ -1,8 +0,0 @@
-require "./connect_db.rb"
-connect_db!
-
-ActiveRecord::Migration.drop_table(:users) do |u|
-  u.column :user_name, :string
-  u.column :user_email, :string
-  u.column :user_password, :string
-end
