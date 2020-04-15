28c762a5b0a14a631d6a981c95a2341ed35193b9
amarnath-reddy-0-9-1-2 <122003102@sastra.ac.in>
2020-04-15 20:23:09 +0530

    designed to show pirticular users todos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 
   def index
     if current_user
-      redirect_to_todos_path
+      redirect_to todos_path
     else
       render "index"
     end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,13 @@
 class TodosController < ApplicationController
   def index
     # render plain: Todo.all.map { |todo| todo.to_pleasent_string }.join("\n")
-
+    @todos = current_user.todos
     render "index"
   end
 
   def show
     id = params[:id]
-    todo = Todo.find(id)
+    todo = current_user.todos.find(id)
     # render plain: todo.to_pleasent_string
     render "todo"
   end
@@ -19,6 +19,7 @@ class TodosController < ApplicationController
       todo_text: todo_text,
       due_date: due_date,
       completed: false,
+      user_id: current_user.id,
     )
     redirect_to todos_path
   end
@@ -26,7 +27,7 @@ class TodosController < ApplicationController
   def update
     id = params[:id]
     completed = params[:completed]
-    todo = Todo.find(id)
+    todo = current_user.todos.find(id)
     todo.completed = completed
     todo.save!
     redirect_to todos_path
@@ -34,7 +35,7 @@ class TodosController < ApplicationController
 
   def destroy
     id = params[:id]
-    todo = Todo.find(id)
+    todo = current_user.todos.find(id)
     todo.destroy
     redirect_to todos_path
   end
